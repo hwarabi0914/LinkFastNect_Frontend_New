49ff2374ed06d4421d572b2259b5d08d27953033
warabi <h.warabi0914@gmail.com>
2025-03-14 07:48:52 +0900

    Fix: next.config.jsのAPIエンドポイント

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const nextConfig = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   },
   output: "standalone",  // 👈 これを追加
 };
