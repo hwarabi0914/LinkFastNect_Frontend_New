63cd04c6d550ae2454dfeb6c92e9c35521736f5e
warabi <h.warabi0914@gmail.com>
2025-03-14 07:39:45 +0900

    Fix: Apply Suspense to useSearchParams in delete/[id]/page.js

--- frontend/src/app/customers/delete/[id]/confirm/page.jsx
+++ frontend/src/app/customers/delete/[id]/confirm/page.jsx
@@ -1,15 +1,24 @@
 "use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function ConfirmPage(props) {
+export default function ConfirmPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ConfirmComponent />
+    </Suspense>
+  );
+}
+
+function ConfirmComponent() {
   const customer_id = useSearchParams().get("customer_id");
 
   return (
     <>
       <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
         <div className="alert alert-success p-4 text-center text-white">
-          {customer_id}を削除しました
+          {customer_id} を削除しました
         </div>
         <Link href="/customers" className="flex justify-center">
           <button className="btn btn-outline btn-accent">一覧に戻る</button>
